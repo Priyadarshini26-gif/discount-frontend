94b7474ae7ecedef313190a63a6a03f20116fc8e
Priyadarshini26-gif <priyadarshini416@gmail.com>
2026-06-16 20:11:24 +0530

    fix deploy error commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://crs-backend-o2de.onrender.com",
+  baseURL: "https://discount-backend-sjs2.onrender.com",
   withCredentials: true
 });
 

78489af2ea9fb1552ed30c7e377e89f2ddb4bf4e
Priyadarshini26-gif <priyadarshini416@gmail.com>
2026-06-16 20:52:34 +0530

    changing baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://discount-backend-sjs2.onrender.com",
+  baseURL: "https://discount-backend-1.onrender.com",
   withCredentials: true
 });
 
